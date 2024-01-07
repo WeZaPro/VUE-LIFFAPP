4fde02ec5c08fc7bfaf1262897146f58bc227cb8
Taweesak.Y <36216629+WeZaPro@users.noreply.github.com>
2024-01-07 19:31:02 +0700

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -37,7 +37,7 @@ export default {
     let uri = window.location.search.substring(1);
     let params = new URLSearchParams(uri);
     liff
-      .init({ liffId: "1656824759-lQKpOazZ" })
+      .init({ liffId: "1656824759-lQKpOazZ",withLoginOnExternalBrowser })
       .then(() => {
         if (!liff.isLoggedIn()) {
           liff.login();
